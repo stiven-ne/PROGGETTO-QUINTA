82e01488d302e5d194ad7b9ea4b06b95ec697e12
stiven-ne <kurtulaj.stiven.studente@itispaleocapa.it>
2026-05-12 23:31:40 +0200

    Delete backend directory

--- backend/routes/budgetRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { verifyTenant } = require('../middleware/tenant');
-const { checkBudgetStatus } = require('../controllers/budgetController');
-
-router.get('/status', protect, verifyTenant, checkBudgetStatus);
-
-module.exports = router;
